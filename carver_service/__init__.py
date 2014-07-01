e930f04fbba91ec03226b48f48357c140a1c2ee0
Wesley Shields <wxs@atarininja.org>
2014-07-01 13:53:27 -0400

    Make carver service work again.

--- carver_service/__init__.py
+++ carver_service/__init__.py
@@ -1,6 +1,10 @@
 import hashlib
 
-from crits.services.core import Service, ServiceConfigOption
+from django.template.loader import render_to_string
+
+from crits.services.core import Service
+
+from . import forms
 
 DEFAULT_START = 0
 DEFAULT_END = 0
@@ -10,31 +14,31 @@ class CarverService(Service):
     version = '0.0.1'
     supported_types = ['Sample']
     description = "Carve a chunk out of a sample."
-    default_config = [
-        ServiceConfigOption('start_offset',
-                            ServiceConfigOption.INT,
-                            description="Start offset",
-                            required=False,
-                            private=False,
-                            default=DEFAULT_START),
-
-        ServiceConfigOption('end_offset',
-                            ServiceConfigOption.INT,
-                            description="End offset",
-                            required=False,
-                            private=False,
-                            default=DEFAULT_END),
-    ]
 
     @staticmethod
     def valid_for(obj):
-        return not obj.filedata.grid_id == None
+        if obj.filedata.grid_id == None:
+            raise ServiceConfigError("Missing filedata.")
 
-    def _scan(self, obj):
-        start_offset = self.config.get("start_offset", DEFAULT_START)
-        end_offset = self.config.get("end_offset", DEFAULT_END)
+    @staticmethod
+    def bind_runtime_form(analyst, config):
+        # The values are submitted as a list for some reason.
+        data = {'start': config['start'][0], 'end': config['end'][0]}
+        return forms.CarverRunForm(data)
+
+    @classmethod
+    def generate_runtime_form(self, analyst, config, crits_type, identifier):
+        return render_to_string('services_run_form.html',
+                                {'name': self.name,
+                                 'form': forms.CarverRunForm(),
+                                 'crits_type': crits_type,
+                                 'identifier': identifier})
+
+    def scan(self, obj, config):
+        start_offset = config['start']
+        end_offset = config['end']
         # Start must be 0 or higher. If end is greater than zero it must
-        # also be greater than end_offset.
+        # also be greater than start_offset.
         if start_offset < 0 or (end_offset > 0 and start_offset > end_offset):
             self._error("Invalid offsets.")
             return
