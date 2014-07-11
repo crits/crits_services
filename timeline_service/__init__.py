f14368f7f5a502b640671693e1fd69d636aedb32
Wesley Shields <wxs@atarininja.org>
2014-07-11 16:05:31 -0400

    Port timeline service.

--- timeline_service/__init__.py
+++ timeline_service/__init__.py
@@ -7,11 +7,8 @@ logger = logging.getLogger(__name__)
 class TimelineService(Service):
     name = "timeline_service"
     version = '0.0.1'
-    type_ = Service.TYPE_CUSTOM
-    template = None
     supported_types = []
     description = "Generate a timeline for an object."
-    default_config = []
 
     def __init__(self, *args, **kwargs):
         pass
