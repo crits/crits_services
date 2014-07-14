66a1e0cb9e30d71a7e242cd7feb1224dd4506fd6
Shayne Snow <ssnow@mitre.org>
2014-07-14 13:40:36 -0400

    Fix indent again?

--- taxii_service/handlers.py
+++ taxii_service/handlers.py
@@ -189,8 +189,8 @@ def run_taxii_service(analyst, obj, rcpts, preview, relation_choices=[], confirm
     # If dealing with an event context, make sure at least one related item is
     # selected. Events have no real sharing value without related information.
     if obj._meta['crits_type'] == Event._meta['crits_type'] and len(relation_choices) == 0:
-            ret['reason'] = "Need at least one related item to send."
-            return ret
+        ret['reason'] = "Need at least one related item to send."
+        return ret
 
     # Get config and grab some stuff we need.
     sc = manager.get_config('taxii_service')
