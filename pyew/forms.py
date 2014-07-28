710f6aff8414df8914756dfb3db44ab89a5ecf50
Wesley Shields <wxs@atarininja.org>
2014-07-28 11:17:40 -0400

    Fixup pyew service.
    Make the config form use label and help_text properly.
    
    Also, fix get_config() and use get_config_details().

--- pyew/forms.py
+++ pyew/forms.py
@@ -5,13 +5,16 @@ class pyewConfigForm(forms.Form):
     required_css_class = 'required'
     pyew = forms.CharField(required=True,
                            initial='',
-                           label = "Full path to pyew py file.")
+                           label='Pyew Script',
+                           help_text="Full path to pyew py file.")
     port = forms.CharField(required=True,
                            initial='9876',
-                           label="Port the pyew websocket is listening on.")
+                           label='Port',
+                           help_text="Port the pyew websocket is listening on.")
     secure = forms.BooleanField(required=False,
                                 initial=True,
-                                label="Use secure websockets.")
+                                label='HTTPs',
+                                help_text="Use secure websockets.")
 
     def __init__(self, *args, **kwargs):
         super(pyewConfigForm, self).__init__(*args, **kwargs)
