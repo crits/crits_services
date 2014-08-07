149192f134e8a9aec6f5f804f97dd9fb3a94176e
Wesley Shields <wxs@atarininja.org>
2014-08-07 15:50:57 -0400

    Port threatrecon service.

---
+++ threatrecon_service/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+class ThreatreconConfigForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    tr_api_key = forms.CharField(required=True,
+                                 label="API Key",
+                                 widget=forms.TextInput(),
+                                 help_text="Obtain API key from Threatrecon.",
+                                 initial='')
+    tr_query_url = forms.CharField(required=True,
+                                   label="Query URL",
+                                   widget=forms.TextInput(),
+                                   initial='https://api.threatrecon.co/api/v1/search')
+
+    def __init__(self, *args, **kwargs):
+        super(ThreatreconConfigForm, self).__init__(*args, **kwargs)
