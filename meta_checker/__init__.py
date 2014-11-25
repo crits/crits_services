a118f777a611dc1fa897593ed035bcb2f4948954
Wesley Shields <wxs@atarininja.org>
2014-11-25 15:29:50 -0500

    Fix meta_checker after AnalysisResult split.

--- meta_checker/__init__.py
+++ meta_checker/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from crits.services.core import Service, ServiceConfigError
 from crits.samples.sample import Sample
+from crits.services.analysis_result import AnalysisResult
 
 class MetaChecker(Service):
     """
@@ -15,7 +16,10 @@ class MetaChecker(Service):
 
     @staticmethod
     def valid_for(obj):
-        if len(obj.analysis) == 0:
+        # Make sure there are AnalysisResults for this object.
+        results = AnalysisResult.objects(object_type=obj._meta['crits_type'],
+                                         object_id=str(obj.id))
+        if len(results) == 0:
             raise ServiceConfigError("Object must have analysis results.")
 
     @staticmethod
@@ -26,24 +30,25 @@ class MetaChecker(Service):
 
     def run(self, obj, config):
         my_md5 = obj.md5
-        my_results = obj.analysis
+        my_results = AnalysisResult.objects(object_type=obj._meta['crits_type'],
+                                            object_id=str(obj.id))
 
         completed_results = []
-        for result_set in my_results:
+        for result_doc in my_results:
             # skip our own results so we don't get nasty feedback
-            if result_set["service_name"] == self.name:
+            if result_doc["service_name"] == self.name:
                 continue
 
-            for result in result_set["results"]:
+            for result in result_doc["results"]:
                 if "md5" in result:
                     res_type = "md5"
                 else:
                     res_type = "result"
-                res_hash = "{0}-{1}".format(result_set["service_name"], result[res_type])
+                res_hash = "{0}-{1}".format(result_doc["service_name"], result[res_type])
                 if result[res_type] and res_hash not in completed_results:
                     total_count = self._get_meta_count(res_type, result[res_type])
                     count_result = {
-                        'service':          result_set["service_name"],
+                        'service':          result_doc["service_name"],
                         'type':             res_type,
                         res_type:           result[res_type],
                         'count':            total_count,
@@ -52,7 +57,8 @@ class MetaChecker(Service):
                     completed_results.append(res_hash)
 
     def _get_meta_count(self, meta_type, meta_val):
-        query_field = "analysis.results.{0}".format(meta_type)
+        query_field = "results.{0}".format(meta_type)
         query = {query_field: meta_val}
-        total_count = Sample.objects(__raw__=query).only('md5').count()
+        total_count = AnalysisResult.objects(object_type='Sample',
+                                             __raw__=query).only('id').count()
         return total_count
