f7f25d3f9c5942ee0be276074a58019799d7e22f
Mike Goffin <mgoffin@mitre.org>
2015-02-13 12:48:26 -0500

    STIX Validator Service.
    This service adds a Nav item under "Services". You can paste a block of
    XML and it will run it through the stix-validator. Any results are
    returned to you.
    
    It currently does not support the "Profile" validation as I think it
    requires updating a Profile file to do the validation with.

---
+++ stix_validator_service/views.py
@@ -0,0 +1,29 @@
+import json
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response, HttpResponse
+from django.contrib.auth.decorators import user_passes_test
+
+from crits.core.user_tools import user_can_view_data
+
+from . import handlers
+
+@user_passes_test(user_can_view_data)
+def validate(request):
+
+    if request.method == "POST" and request.is_ajax():
+        xml = request.POST['xml']
+        results = {'results': handlers.validate_stix(xml)}
+        return HttpResponse(json.dumps(results),
+                            mimetype="application/json")
+    else:
+        return render_to_response("error.html",
+                                  {"error" : 'Expected AJAX POST.'},
+                                  RequestContext(request))
+
+
+@user_passes_test(user_can_view_data)
+def stix_validator(request):
+    return render_to_response("stix_validator.html",
+                              {},
+                              RequestContext(request))
