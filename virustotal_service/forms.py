049793357df80b5eae7e7043454bb2e7f349ce7b
webstergd <webstergd@sec.in.tum.de>
2015-02-15 18:36:26 +0100

    Enhanced VT service to support selected items available with a private key.
    - Will pull private API information for samples.
    - Provides an option to download pcap file.
        - Will add pcap to Crits and form a relationship.
        - Relationship dated from when VT analysis was performed.
    - The service will add domains to crits if detected in samples.
    - Cleaned up the Run() fuction:
        - Logical blocks now live in their own fuction.
        - Added a lot of code comments.
        - Not fully finished with this...but it is a start.
    - Added error checks and output.
    - Unified output for domains and IPs

--- virustotal_service/forms.py
+++ virustotal_service/forms.py
@@ -3,6 +3,14 @@ from django import forms
 class VirusTotalConfigForm(forms.Form):
     error_css_class = 'error'
     required_css_class = 'required'
+    vt_api_key_private = forms.BooleanField(required=False,
+                                initial=False,
+                                label='Private key?',
+                                help_text="Is the key a private key?")
+    vt_api_pcap = forms.BooleanField(required=False,
+                                initial=False,
+                                label='Pull pcap?',
+                                help_text="If available, should we pull the pcap file?")
     vt_api_key = forms.CharField(required=True,
                                  label="API Key",
                                  widget=forms.TextInput(),
@@ -20,6 +28,11 @@ class VirusTotalConfigForm(forms.Form):
                                 label="IP URL",
                                 widget=forms.TextInput(),
                                 initial='https://www.virustotal.com/vtapi/v2/ip-address/report')
+    vt_network_url = forms.CharField(required=True,
+                                label="Network URL",
+                                widget=forms.TextInput(),
+                                initial='https://www.virustotal.com/vtapi/v2/file/network-traffic')
+
 
     def __init__(self, *args, **kwargs):
         super(VirusTotalConfigForm, self).__init__(*args, **kwargs)
