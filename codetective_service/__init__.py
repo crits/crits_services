20855a96ba8d1ef7a0ce203945a17fb95d81e9cc
Adam Polkosnik <adam.polkosnik@ny.frb.org>
2015-02-26 11:07:28 -0500

    Update the forms and temporarily use repr() to avoid the decode errors

--- codetective_service/__init__.py
+++ codetective_service/__init__.py
@@ -25,7 +25,7 @@ class CodetectiveService(Service):
     """
 
     name = "codetective"
-    version = '0.0.4'
+    version = '0.0.5'
     supported_types = ['Sample']
     description = "Find password hashes and so on"
 
@@ -35,41 +35,42 @@ class CodetectiveService(Service):
         fields = forms.CodetectiveServiceConfigForm().fields
         for name, field in fields.iteritems():
             config[name] = field.initial
-
+        
         # If there is a config in the database, use values from that.
         if existing_config:
             for key, value in existing_config.iteritems():
                 config[key] = value
         return config
-
+    
     @staticmethod
     def parse_config(config):
         try:
             from Codetective.codetective import get_type_of, Finding
+            return
         except:
-            raise ServiceConfigError("Unable to import Codetective/codetective.py - make sure that there is Codetective/__init__.py present.")
-
+            raise ServiceConfigError("Unable to import Codetective/codetective.py!")
+    
     @staticmethod
     def get_config_details(config):
         display_config = {}
-
+        
         # Rename keys so they render nice.
         fields = forms.CodetectiveServiceConfigForm().fields
         for name, field in fields.iteritems():
             display_config[field.label] = config[name]
-
         return display_config
 
     @staticmethod
     def valid_for(obj):
         if obj.filedata.grid_id == None:
             raise ServiceConfigError("Missing filedata.")
+        return
 
     @classmethod
     def generate_config_form(self, config):
         html = render_to_string('services_config_form.html',
                                 {'name': self.name,
-                                 'form': forms.CodetectiveServiceConfigForm(initial=config),
+                                 'form': forms.CodetectiveServiceConfigForm(),
                                  'config_error': None})
         form = forms.CodetectiveServiceConfigForm
         return form, html
@@ -78,32 +79,36 @@ class CodetectiveService(Service):
     def bind_runtime_form(analyst, config):
         data = {'start_offset': config['start_offset'],
                 'end_offset': config['end_offset'],
-                'filters': config['filters'],
+                'certainty': config['certainty'],
                 'analyze': config['analyze']}
         return forms.CodetectiveServiceRunForm(data)
-        
-#    @classmethod
-#    def generate_runtime_form(self, config):
-#        html = render_to_string('services_run_form.html',
-#                                {'name': self.name,
-#                                 'form': forms.CodetectiveServiceRunForm(initial=config),
-#                                 'config_error': None})
-#        form = forms.CodetectiveServiceRunForm
-#        return form, html
-
+    
+    @classmethod
+    def generate_runtime_form(self, analyst, config, crits_type, identifier):
+        return render_to_string('services_run_form.html',
+                                {'name': self.name,
+                                 'form': forms.CodetectiveServiceRunForm(),
+                                 'crits_type': crits_type,
+                                 'identifier': identifier})
+    
     def run(self, obj, config):
         start_offset = config['start_offset']
         end_offset = config['end_offset']
         analyze = config['analyze']
+        certainty = config['certainty']
         filters = config['filters']
-        try:
-            self._log('info',"filters:%s analyze:%x" %(filters, analyze))
-            results = get_type_of(obj.filedata.read()[start_offset:end_offset], filters)
-            results = [finding for finding in results if finding.certainty >= 45]
-            for finding in results:
-                self._add_result('Codetective', "%s" % str(finding.payload), {'Offset': finding.location, 'Type': finding.type, 'Confidence': finding.confidence, 'Certainty': finding.certainty, 'Details':str(finding.details).encode('ascii'), 'Datestamp': finding.created_on})
-        except Exception as err:
-            self._info("Codetective failed on: %s: %s" % (str(obj.id), err))
-            return
-        self._info("Finished Codetective on: %s" % str(obj.id) )
-
+        data = obj.filedata.read()[start_offset:end_offset]
+        if not data:
+            self._error("Codetective received no data.")
+            raise ("Codetective received no data.")
+        else:
+            try:
+                self._info("Codetective filters:%s analyze:%x, certainty:%d" %(filters, analyze, certainty))
+                results = get_type_of(data, filters)
+                results = [finding for finding in results if finding.certainty >= certainty]
+                for finding in results:
+                    self._add_result('Codetective', "%s" % repr(finding.payload), {'Offset': finding.location, 'Type': finding.type, 'Confidence': finding.confidence, 'Certainty': finding.certainty, 'Details':repr(finding.details), 'Datestamp': finding.created_on})
+            except Exception as err:
+                self._info("Codetective failed on: %s: %s" % (str(obj.id), err))
+            self._info("Codetective finished on: %s" % str(obj.id) )
+        return
