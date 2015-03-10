5a11c51d597a45ab5dd9970a158c42f6533d2574
Wootski <wootski@users.noreply.github.com>
2015-03-10 10:52:03 -0400

    Stream extraction and JavaScript detection - Walk PDF objects to detect JavaScript - Attempt to extract all streams - Produce stats on streams
    Output changes
    - Reverted back to object.id as row title
        - MD5 not useful is objects are not added to CRITS

--- pdfinfo_service/__init__.py
+++ pdfinfo_service/__init__.py
@@ -63,20 +63,59 @@ class PDFInfoService(Service):
         except KeyError:
             pass
 
+    def js_ref_search(self, data):
+        """
+        Locate PDF objects that reference JavaScript
+        @return list of object id's that contain JS
+        - This could be achieved using PeePDF.
+        """
+        oPDFParser = pdfparser.cPDFParser(data)
+        done = False 
+        js_items = []
+
+        while done == False:
+            try:
+                pdf_object = oPDFParser.GetObject()
+            except Exception as e:
+                pdf_object = None
+
+            if pdf_object != None:
+                if pdf_object.type in [pdfparser.PDF_ELEMENT_INDIRECT_OBJECT]:
+                    rawContent = pdfparser.FormatOutput(pdf_object.content, True)
+                    pdf_references = pdf_object.GetReferences()
+                    if pdf_references:
+                        #Search for JavaScript references and match with GetReferences()
+                        tags = re.findall(r'\/JavaScript\s(\d+)\s\d+\sR',rawContent[:100])
+                        tags += re.findall(r'\/JS\s(\d+)\s\d+\sR',rawContent[:100])
+                        for tag in tags:
+                            for ref in pdf_references:
+                                if tag == int(ref[0]):
+                                    js_items.append(tag)
+                    else:
+                        if (pdf_object.Contains('/JavaScript') | pdf_object.Contains('/JS')):
+                            js_items.append(pdf_object.id)
+            else:
+                done = True
+        return js_items               
+
     def run_pdfparser(self, data):
         """
         Uses pdf-parser to get information for each object.
         """        
         oPDFParser = pdfparser.cPDFParser(data)
         self._debug("Parsing document")
-        done = True
+        done = False
+        js_items = []
 
-        while done == True:
+        #Walk objects and look for JavaScript
+        js_items = self.js_ref_search(data)
+
+        while done == False:
             try:
                 pdf_object = oPDFParser.GetObject()
             except Exception as e:
                 pdf_object = None
- 
+
             if pdf_object != None:
                 if pdf_object.type in [pdfparser.PDF_ELEMENT_INDIRECT_OBJECT]:
                     rawContent = pdfparser.FormatOutput(pdf_object.content, True)
@@ -85,31 +124,48 @@ class PDFInfoService(Service):
                     object_type = pdf_object.GetType()
 
                     object_stream = False
+                    streamContent = ''
+                    stream_md5_digest = ''
+                    stream_entropy = ''
                     if pdf_object.ContainsStream():
                         object_stream = True
+                        try:
+                            #decompress stream using codec
+                            streamContent = pdf_object.Stream() 
+                        except Exception as e:
+                            streamContent = "decompress failed."
+
+                        if "decompress failed." in streamContent[:50]:
+                            #Provide raw stream data
+                            streamContent = pdf_object.Stream('')
+                        stream_md5_digest = hashlib.md5(streamContent).hexdigest()
+                        stream_entropy = self.H(streamContent)
 
                     object_references = []
                     for reference in pdf_object.GetReferences():
                         object_references.append(reference[0])
                     object_references = ','.join(object_references)
 
-                    js_tags = str(pdf_object.Contains('/JavaScript') | pdf_object.Contains('/JS'))
+                    js_found = False
+                    if pdf_object.id in js_items:
+                        js_found = True
 
-                    row_title = "{} ({})".format(object_type,section_md5_digest)
                     result = {
                             "obj_id":           pdf_object.id,
                             "obj_version":      pdf_object.version,
                             "size":             len(rawContent),
+                            "obj_md5":          section_md5_digest,
                             "type":             object_type,
-                            "md5":              section_md5_digest,
                             "entropy":          section_entropy,
-                            "javascript_tags":  js_tags,
+                            "javascript":       str(js_found),
                             "obj_references":   object_references,
                             "obj_stream":       object_stream,
+                            "stream_md5":       stream_md5_digest,
+                            "stream_entropy":   stream_entropy,
                     }
-                    self._add_result('pdf_parser', row_title, result)
+                    self._add_result('pdf_parser', pdf_object.id, result)
             else:
-                done = False
+                done = True
 
     def run(self, obj, config):
         """
