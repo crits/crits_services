018980ef3cfdc5f95fe79f7a1f7dd9bce2ca0985
Mike Goffin <mgoffin@mitre.org>
2015-05-15 15:36:59 -0400

    Add CybOX/STIX to TAXII service.
    The code that was removed from core is now here in the TAXII service.
    This adds the Nav menu item for "STIX Import" along with the form and
    backend support for it. Object Mapper from core has been moved here and
    references to it were changed. The "to_cybox*" and "to_stix*" code that
    were part of the TLO classes are now functions in the handler file.
    
    TAXII service also expands the API to continue support for STIX
    importing through the API.
    
    Once the core vocabularies change we will need to add enhancements for
    mappings between the new vocabulary and the CybOX/STIX counterparts.

--- taxii_service/views.py
+++ taxii_service/views.py
@@ -1,10 +1,9 @@
+import logging
 import json
-import tempfile
 
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.shortcuts import render_to_response, HttpResponse
-from django.core.servers.basehttp import FileWrapper
 from django.contrib.auth.decorators import user_passes_test
 
 from crits.core.class_mapper import class_from_id
@@ -12,6 +11,8 @@ from crits.core.user_tools import user_can_view_data
 
 from . import handlers
 from . import forms
+
+logger = logging.getLogger(__name__)
 
 @user_passes_test(user_can_view_data)
 def taxii_agent(request):
@@ -105,5 +106,68 @@ def get_taxii_result(request, crits_type, crits_id, preview):
     else: # form doesn't validate
         data = {'success': False, 'reason': "Invalid options provided. Please fix and try again."}
         return HttpResponse(json.dumps(data), mimetype="application/json")
-    
 
+@user_passes_test(user_can_view_data)
+def upload_standards(request):
+    """
+    Upload a standards document.
+
+    :param request: Django request.
+    :type request: :class:`django.http.HttpRequest`
+    :returns: :class:`django.http.HttpResponse`
+    """
+
+    std_form = forms.UploadStandardsForm(request.user, request.POST, request.FILES)
+    response = {
+                   'form': std_form.as_table(),
+                   'success': False,
+                   'message': ""
+                 }
+
+    if request.method != "POST":
+        response['message'] = "Must submit via POST."
+        return render_to_response('file_upload_response.html',
+                                  {'response': json.dumps(response)},
+                                  RequestContext(request))
+
+    if not std_form.is_valid():
+        response['message'] = "Form is invalid."
+        return render_to_response('file_upload_response.html',
+                                  {'response': json.dumps(response)},
+                                  RequestContext(request))
+
+    data = ''
+    for chunk in request.FILES['filedata']:
+        data += chunk
+
+    make_event = std_form.cleaned_data['make_event']
+    source = std_form.cleaned_data['source']
+
+    reference = std_form.cleaned_data['reference']
+
+
+    # XXX: Add reference to form and handle here?
+    status = handlers.import_standards_doc(data, request.user.username, "Upload",
+                                 ref=reference, make_event=make_event, source=source)
+
+    if not status['success']:
+        response['message'] = status['reason']
+        return render_to_response('file_upload_response.html',
+                                  {'response': json.dumps(response)},
+                                  RequestContext(request))
+
+    response['success'] = True
+    response['message'] = render_to_string("import_results.html", {'failed' : status['failed'], 'imported' : status['imported']})
+    return render_to_response('file_upload_response.html',
+                              {'response': json.dumps(response)},
+                              RequestContext(request))
+
+def taxii_service_context(request):
+    context = {}
+    if request.user.is_authenticated():
+        user = request.user.username
+        try:
+            context['upload_standards'] = forms.UploadStandardsForm(user)
+        except Exception, e:
+            logger.warning("Base Context UploadStandardsForm Error: %s" % e)
+    return context
