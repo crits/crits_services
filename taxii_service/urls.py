4a1111451fea0d647661e4a38fc544d722429905
Mike Goffin <mgoffin@mitre.org>
2015-05-15 15:36:59 -0400

    Add CybOX/STIX to TAXII service.
    The code that was removed from core is now here in the TAXII service.
    This adds the Nav menu item for "STIX Import" along with the form and
    backend support for it. Object Mapper from core has been moved here and
    references to it were changed. The "to_cybox*" and "to_stix*" code that
    were part of the TLO classes are now functions in the handler file.
    
    TAXII service also expands the API to continue support for STIX
    importing through the API.
    
    Once the core vocabularies change we will need to add enhancements for
    mappings between the new vocabulary and the CybOX/STIX counterparts.

--- taxii_service/urls.py
+++ taxii_service/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns
 
+def register_api(v1_api):
+    from taxii_service.api import StandardsResource
+    v1_api.register(StandardsResource())
+
 urlpatterns = patterns('taxii_service.views',
     (r'^taxii_agent/$', 'taxii_agent'),
     (r'^get_taxii_config_form/(?P<crits_type>\S+)/(?P<crits_id>\S+)/$', 'get_taxii_config_form'),
     (r'^(?P<crits_type>\S+)/(?P<crits_id>\S+)/$', 'execute_taxii_service'),
     (r'^(?P<crits_type>\S+)/(?P<crits_id>\S+)/preview$', 'preview_taxii_service'),
+    (r'^upload/$', 'upload_standards'),
 )
