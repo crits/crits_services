2df282357df9352ac65f21ab58cb1fc7c17d8047
Mike Goffin <mgoffin@mitre.org>
2015-09-10 13:38:48 -0400

    Remove unused imports.

--- unswf_service/__init__.py
+++ unswf_service/__init__.py
@@ -1,7 +1,6 @@
 # (c) 2015, Adam Polkosnik <adam.polkosnik@ny.frb.org>
 #
 import logging
-import os
 import io
 import zlib
 import pylzma
@@ -12,8 +11,6 @@ from hashlib import md5
 # for adding the extracted files
 from crits.samples.handlers import handle_file
 
-from django.conf import settings
-from django.template.loader import render_to_string
 from crits.services.core import Service, ServiceConfigError
 
 from crits.vocabulary.relationships import RelationshipTypes
@@ -21,11 +18,11 @@ from crits.vocabulary.relationships import RelationshipTypes
 
 logger = logging.getLogger(__name__)
 class unswfService(Service):
-     
+
     """
     Uncompress flash files.
     """
-     
+
     name = "unswf"
     version = '0.0.2'
     supported_types = ['Sample']
