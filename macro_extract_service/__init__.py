8300c5120dc9db9cd9374298b1376339d3bdc467
Mike Goffin <mgoffin@mitre.org>
2016-02-05 14:24:43 -0500

    Adding macro_extract service.
    Extracs VBA Macro code from office files.

---
+++ macro_extract_service/__init__.py
@@ -0,0 +1,111 @@
+from oletools.olevba import (
+    VBA_Parser
+)
+
+from crits.raw_data.handlers import handle_raw_data_file
+from crits.services.core import Service, ServiceConfigError
+from crits.vocabulary.relationships import RelationshipTypes
+
+
+class MacroExtractService(Service):
+    """
+    Attempts to extract VBA Macros from MS Office files.
+    """
+
+    name = "macro_extract"
+    version = '0.1.0'
+    supported_types = ['Sample']
+    description = "Extracs VBA Macros from MS Office documents."
+
+    @staticmethod
+    def get_config(existing_config):
+        # This service no longer uses config options, so blow away any existing
+        # configs.
+        return {}
+
+    @staticmethod
+    def valid_for(obj):
+        office_magic = "\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1"
+        if obj.filedata != None:
+            data = obj.filedata.read()
+            # Need to reset the read pointer.
+            obj.filedata.seek(0)
+            if data.startswith(office_magic):
+                return
+        raise ServiceConfigError("Not a valid office document.")
+
+    def run(self, obj, config):
+        username = self.current_task.username
+        filename = obj.filename
+        filedata = obj.filedata.read()
+        try:
+            vbaparser = VBA_Parser(filename, data=filedata)
+        except Exception, e:
+            print str(e)
+            return
+        if vbaparser.detect_vba_macros():
+            for (filename, stream_path, vba_filename, vba_code) in vbaparser.extract_macros():
+                d = {
+                    'OLE stream': stream_path,
+                    'VBA filename': vba_filename
+                }
+                #self._add_result('macro_extract', 'code', {'code': vba_code})
+                result = handle_raw_data_file(
+                    vba_code,
+                    obj.source,
+                    user=username,
+                    description="VBA Macro source code for %s" % vba_filename,
+                    title=vba_filename,
+                    data_type="Text",
+                    tool_name=self.name,
+                    tool_version=self.version,
+                    tool_details=self.description
+                )
+                if result['success']:
+                    obj.add_relationship(
+                        result['object'],
+                        RelationshipTypes.RELATED_TO,
+                        analyst=username,
+                        rel_reason="Extracted from related Sample"
+                    )
+                    obj.save()
+                    d['RawData'] = result['_id']
+                self._add_result('Macros', filename, d)
+            results = vbaparser.analyze_macros(show_decoded_strings=True)
+            self._add_result('Counts',
+                             'Suspicious keywords',
+                             {'Count': vbaparser.nb_suspicious})
+            self._add_result('Counts',
+                             'AutoExec keywords',
+                             {'Count': vbaparser.nb_autoexec})
+            self._add_result('Counts',
+                             'IOCs',
+                             {'Count': vbaparser.nb_iocs})
+            self._add_result('Counts',
+                             'Hex obfuscated strings',
+                             {'Count': vbaparser.nb_hexstrings})
+            self._add_result('Counts',
+                             'Base64 obfuscated strings',
+                             {'Count': vbaparser.nb_base64strings})
+            self._add_result('Counts',
+                             'Dridex obfuscated strings',
+                             {'Count': vbaparser.nb_dridexstrings})
+            self._add_result('Counts',
+                             'VBA obfuscated strings',
+                             {'Count': vbaparser.nb_vbastrings})
+            for kw_type, keyword, description in results:
+                try:
+                    d = {
+                        'keyword': keyword.decode('utf-8'),
+                        'description': description.decode('utf-8')
+                    }
+                    self._add_result('Keywords', kw_type, d)
+                except:
+                    pass
+        else:
+            print 'No VBA Macros found'
+        vbaparser.close()
+        return
+
+    def _parse_error(self, item, e):
+        self._error("Error parsing %s (%s): %s" % (item, e.__class__.__name__, e))
