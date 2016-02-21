4fd270bc175cc3b22a54ee2a6fe047b0af3367e7
Brandon Dixon <brandon@9bplus.com>
2016-02-21 18:21:45 +0000

    Working PassiveTotal service

--- passivetotal_service/forms.py
+++ passivetotal_service/forms.py
@@ -1,18 +1,40 @@
 from django import forms
 
+
 class PassiveTotalConfigForm(forms.Form):
     error_css_class = 'error'
     required_css_class = 'required'
-    pt_api_key = forms.CharField(required=True,
-                                 label="API Key",
-                                 widget=forms.TextInput(),
-                                 help_text="Obtain API key from PassiveTotal.",
-                                 initial='')
-    pt_query_url = forms.CharField(required=True,
-                                   label="Query URL",
-                                   widget=forms.TextInput(),
-                                   initial='https://www.passivetotal.org/api/v1')
+    pt_username = forms.CharField(
+        required=True,
+        label="Username",
+        widget=forms.TextInput(),
+        help_text="Email address used to login to PassiveTotal.",
+        initial=''
+    )
+    pt_api_key = forms.CharField(
+        required=True,
+        label="API Key",
+        widget=forms.TextInput(),
+        help_text="Obtain API key from PassiveTotal.",
+        initial=''
+    )
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', ':')
         super(PassiveTotalConfigForm, self).__init__(*args, **kwargs)
+
+
+class PassiveTotalRuntimeForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', ':')
+        super(PassiveTotalRuntimeForm, self).__init__(*args, **kwargs)
+
+        self.fields['dns'] = forms.BooleanField(
+            required=False,
+            initial=True,
+            label="Passive DNS",
+            help_text="Perform a passive DNS query."
+        )
