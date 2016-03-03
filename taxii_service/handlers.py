3b0d57e6e88f3223bba90f638875a212d803c35e
Bradley Logan <bradley.r.logan@boeing.com>
2016-03-03 14:52:01 -0800

    TAXII: Add support for HTTP connections through a proxy

--- taxii_service/handlers.py
+++ taxii_service/handlers.py
@@ -267,7 +267,10 @@ def execute_taxii_agent(hostname=None, https=None, port=None, path=None,
         proxy = settings.HTTP_PROXY
         if not proxy.startswith('http://'):
             proxy = 'http://' + proxy
-        client.setProxy(proxy, proxy_type=tc.HttpClient.PROXY_HTTPS)
+        if https:
+            client.setProxy(proxy, proxy_type=tc.HttpClient.PROXY_HTTPS)
+        else:
+            client.setProxy(proxy, proxy_type=tc.HttpClient.PROXY_HTTP)
 
     crits_taxii = taxii.Taxii()
     crits_taxii.runtime = runtime
@@ -1145,7 +1148,10 @@ def run_taxii_service(analyst, obj, rcpts, preview,
         proxy = settings.HTTP_PROXY
         if not proxy.startswith('http://'):
             proxy = 'http://' + proxy
-        client.setProxy(proxy, proxy_type=tc.HttpClient.PROXY_HTTPS)
+        if https:
+            client.setProxy(proxy, proxy_type=tc.HttpClient.PROXY_HTTPS)
+        else:
+            client.setProxy(proxy, proxy_type=tc.HttpClient.PROXY_HTTP)
 
     # The minimum required info has been provided by user via the TAXII form.
     # Form configuration and validation ensures the form is valid.
