f07585d8c6af4c0099fdb9e7354bb711566f5563
Mike Goffin <mgoffin@mitre.org>
2016-03-29 16:48:33 -0400

    Add ability to manage privacy groups for ThreatExchange.
    Adds a separate interface which can be accessed from the nav menu which
    allows you to manage your privacy groups. You can see the groups you are
    in, and for those which you are an owner you can modify them as you see
    fit.

--- threatexchange/urls.py
+++ threatexchange/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('threatexchange.views',
     (r'^query/$', 'query'),
+    (r'^privacy_groups/$', 'privacy_groups'),
     (r'^submit_query/$', 'submit_query'),
     (r'^submit_related_query/$', 'submit_related_query'),
     (r'^export_object/$', 'export_object'),
@@ -9,4 +10,6 @@ urlpatterns = patterns('threatexchange.views',
     (r'^get_members/$', 'get_members'),
     (r'^get_groups/$', 'get_groups'),
     (r'^get_dropdowns/$', 'get_dropdowns'),
+    (r'^get_privacy_group_form/$', 'get_privacy_group_form'),
+    (r'^add_edit_privacy_group/$', 'add_edit_privacy_group'),
 )
