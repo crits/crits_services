980897a5d9edd12bf894e703a07bdf662b07e25a
Lance Mueller <ller@semprautilities.com>
2017-01-14 19:01:47 -0800

    Edits

--- bit9_service/__init__.py
+++ bit9_service/__init__.py
@@ -14,7 +14,7 @@ class Bit9Service(Service):
     @staticmethod
     def save_runtime_config(config):
         del config['bit9_api_key']
-	del config['bit9_server']
+        del config['bit9_server']
 
     @staticmethod
     def get_config(existing_config):
@@ -34,11 +34,11 @@ class Bit9Service(Service):
     def parse_config(config):
         if not config['bit9_api_key']:
             raise ServiceConfigError("API key required.")
-	if not config['bit9_server']:
-	    raise ServiceConfigError("Bit9 Server required.")
-	else:
-	    if 'https://' not in config.get('bit9_server','') and 'http://' not in config.get('bit9_server',''):
-		 raise ServiceConfigError("Bit9 Server required, include the http:// or https://") 
+        if not config['bit9_server']:
+            raise ServiceConfigError("Bit9 Server required.")
+        else:
+            if 'https://' not in config.get('bit9_server','') and 'http://' not in config.get('bit9_server',''):
+                raise ServiceConfigError("Bit9 Server required, include the http:// or https://") 
 
     @classmethod
     def generate_config_form(self, config):
@@ -61,80 +61,82 @@ class Bit9Service(Service):
 
         return display_config
 
-    def run(self, obj, config):
-	try:
-	    key = config.get('bit9_api_key', '')
-	    server = config.get('bit9_server','')
-	    self.get_hash(key,server, obj)
 
-	except Exception as e:
-	    self._error("Error: %s" % str(e))
+    def run(self, obj, config):
+        try:
+            key = config.get('bit9_api_key', '')
+            server = config.get('bit9_server','')
+            self.get_hash(key,server, obj)
+
+        except Exception as e:
+            self._error("Error: %s" % str(e))
 
     def get_hash(self, apikey, server, obj):
-	try: 
-		url =''
-		if obj._meta['crits_type'] == 'Indicator':
-		    if obj['ind_type'] == 'MD5':
-			url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=md5:",obj['value']))
-			
-		    elif obj['ind_type'] == 'SHA1':    
-                        url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=sha1:",obj['value']))
+        try: 
+            url =''
+            if obj._meta['crits_type'] == 'Indicator':
+                if obj['ind_type'] == 'MD5':
+                    url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=md5:",obj['value']))
+                
+                elif obj['ind_type'] == 'SHA1':    
+                    url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=sha1:",obj['value']))
 
-                    elif obj['ind_type'] == 'SHA256':    
-                        url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=sha256:",obj['value']))
+                elif obj['ind_type'] == 'SHA256':    
+                    url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=sha256:",obj['value']))
 
-		    else:
-			self._add_result("Service Sucessfully ran", "Indicator type is not searchable in BIT9" )
-		
-		elif obj._meta['crits_type'] == 'Sample':
-		    url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=md5:",obj.md5))
+                else:
+                    self._add_result("Service Sucessfully ran", "Indicator type is not searchable in BIT9" )
+                    return
+            
+            elif obj._meta['crits_type'] == 'Sample':
+                url = ''.join((server,"/api/bit9platform/v1/filecatalog?q=md5:",obj.md5))
 
-		else:
-		    return
+            else:
+                return
 
-        	headers = {'X-Auth-Token': apikey}
-        	self._info("Searching Bit9.")
-        	response = requests.get(url,headers=headers, verify=False)
-        	self._info("Filecatalog query status code: {0}".format(response.status_code))
-        	if response.status_code == 200:
-		    all_json = response.json()
-            	    if all_json:
-                	if 'computerId' in all_json[0]:
-                    	    hostname = self.get_computer(server,apikey,all_json[0]['computerId'])
-                    	    if hostname is not None:
-                        	d = {'State': all_json[0]['effectiveState'],'Filename': all_json[0]['fileName'], 'Path': all_json[0]['pathName'], 'Publisher': all_json[0]['publisher'], 'ProductName': all_json[0]['productName'], 'Threat': all_json[0]['threat'], 'Filesize': all_json[0]['fileSize']}
-                        	self._add_result('Bit9 Data',hostname,d )
-                    	    else:
-                        	self._error("Error determining hostname!")
-                	else:
-                    	  self._error("\'computerId\' key was not found!")
-            	    else:
-                	self._info("No result found!")       
-        	else:
-            	  self._error("Error: {0}".format(response.status_code))
+            headers = {'X-Auth-Token': apikey}
+            self._info("Searching Bit9.")
+            response = requests.get(url,headers=headers, verify=False)
+            self._info("Filecatalog query status code: {0}".format(response.status_code))
+            if response.status_code == 200:
+                all_json = response.json()
+                if all_json:
+                    if 'computerId' in all_json[0]:
+                        hostname = self.get_computer(server,apikey,all_json[0]['computerId'])
+                        if hostname is not None:
+                            data = {'State': all_json[0]['effectiveState'],'Filename': all_json[0]['fileName'], 'Path': all_json[0]['pathName'], 'Publisher': all_json[0]['publisher'], 'ProductName': all_json[0]['productName'], 'Threat': all_json[0]['threat'], 'Filesize': all_json[0]['fileSize']}
+                            self._add_result('Bit9 Data',hostname,data)
+                        else:
+                            self._error("Error determining hostname!")
+                    else:
+                          self._error("\'computerId\' key was not found!")
+                else:
+                    self._info("No result found!")       
+            else:
+                  self._error("Error: {0}".format(response.status_code))
 
-	except Exception as e:
-            self._error("Error: %s" % str(e)) 	
+        except Exception as e:
+            self._error("Error: %s" % str(e))   
 
     def get_computer(self, server, apikey, id):
-	try:
-        	url = server + "/api/bit9platform/v1/computer?q=id:" + str(id)
-        	headers = {'X-Auth-Token': apikey}
-        	response = requests.get(url,headers=headers, verify=False)
-        	self._info("Computer query status code: {0}".format(response.status_code))
-        	if response.status_code == 200:
-            	    all_json = response.json()
-            	    if all_json:
-                	if 'name' in all_json[0]:
-                    	    return all_json[0]['name']
-                	else:
-                    	    self._error("\'name\' key was not found!")
-            	    else:
-                	self._info("No result found!") 
-        	else:
-            	    self._error("Error: {0}".format(response.status_code))
+        try:
+            url = server + "/api/bit9platform/v1/computer?q=id:" + str(id)
+            headers = {'X-Auth-Token': apikey}
+            response = requests.get(url,headers=headers, verify=False)
+            self._info("Computer query status code: {0}".format(response.status_code))
+            if response.status_code == 200:
+                all_json = response.json()
+                if all_json:
+                    if 'name' in all_json[0]:
+                        return all_json[0]['name']
+                    else:
+                        self._error("\'name\' key was not found!")
+                else:
+                    self._info("No result found!") 
+            else:
+                    self._error("Error: {0}".format(response.status_code))
 
-        	return None
-	
-	except Exception as e:
-            self._error("Error: %s" % str(e)) 
+            return None
+    
+        except Exception as e:
+                self._error("Error: %s" % str(e)) 
--- bit9_service/forms.py
+++ bit9_service/forms.py
@@ -4,22 +4,8 @@ from django import forms
 class Bit9ConfigForm(forms.Form):
     error_css_class = 'error'
     required_css_class = 'required'
-<<<<<<< 3f179aafac25719c29b6d751275f24c25b8fa8c0
-    bit9_api_key = forms.CharField(required=True, 
-                                label="API Key:", 
-                                widget=forms.TextInput(), 
-                                help_text="API key from Bit9.",
-                                initial='')
-                                
-    bit9_server = forms.CharField(required=True, 
-                                label="Bit9 Server URL:", 
-                                widget=forms.TextInput(), 
-                                help_text="Bit9 server hostname/IP URL: (https://bit9.myorganization.com).",
-                                initial='')
-=======
     bit9_api_key = forms.CharField(required=True, label="API Key:", widget=forms.TextInput(), help_text="API key from Bit9.",initial='')
     bit9_server = forms.CharField(required=True, label="Bit9 Server URL:", widget=forms.TextInput(), help_text="Bit9 server hostname/IP URL: (https://bit9.myorganization.com).",initial='')
->>>>>>> commit
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', ':')
