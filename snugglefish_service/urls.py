20fad349e363cbb12885b0c95a5c200a6d0b8c21
Mike Goffin <mgoffin@mitre.org>
2017-05-02 11:37:36 -0700

    Removing snugglefish service
    The service breaks some nav stuff, hasn't been kept updated, and the
    backend system that it works with hasn't been worked on in years. I
    suspect this is going to be a dead service and since it breaks the UI it
    needs to be removed.

--- snugglefish_service/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns
-
-urlpatterns = patterns('snugglefish_service.views',
-    (r'^snugglefish_search/$', 'snugglefish_search'),
-    (r'^snugglefish_status/$', 'snugglefish_status'),
-    (r'^get_snugglefish_search_form/$', 'get_snugglefish_search_form'),
-)
