6b0c6cee540d3ea041098cd76502d0d66cfd9267
lionel <lionel@cnamts.local>
2017-07-12 10:09:22 +0200

    Correction of errors

--- extract_embedded_service/__init__.py
+++ extract_embedded_service/__init__.py
@@ -16,7 +16,7 @@ from crits.core.user_tools import get_user_info
 from crits.services.core import Service, ServiceConfigError
 from crits.samples.handlers import handle_file
 from crits.vocabulary.relationships import RelationshipTypes
-from crits.vocabulary.acls import RawDataACL
+from crits.vocabulary.acls import SampleACL
 from crits.core.class_mapper import class_from_id
 from django.conf import settings
 from django.template.loader import render_to_string
@@ -91,8 +91,7 @@ class ExtractEmbeddedService(Service):
         self.obj = obj
         self._debug("ExtractEmbedded started")
         clamscan_path = self.config.get("clamscan_path", "/usr/bin/clamscan")
-        # The _write_to_file() context manager will delete this file at the
-        # end of the "with" block.
+        #write out the sample stored in the db to a tmp file
         with self._write_to_file() as tmp_file:
             (working_dir, filename) = os.path.split(tmp_file)
             new_env = dict(os.environ)  # Copy current environment
@@ -100,49 +99,63 @@ class ExtractEmbeddedService(Service):
             obj.filedata.seek(0)
             #make temp dir for extract embedded file
             dtmp = tempfile.mkdtemp()
-            #create empty file for no check sig on file
-            emptyrule_path = tempfile.gettempdir() + '/emptyrule.yar'
-            if not os.path.isfile(emptyrule_path):
-                open(emptyrule_path, 'a').close()
-            #TODO add filename filter for avoid escape shell
-            args = [clamscan_path, '--quiet', '--leave-temps', '--tempdir=' + dtmp, '-d', emptyrule_path, filename]
-            if not user.has_access_to(RawDataACL.WRITE):
-                self._info("User does not have permission to add Raw Data to CRITs")
-                self._add_result("Parsing Cancelled", "User does not have permission to add Raw Data to CRITs")
-                return
-            # pdftotext does not generate a lot of output, so we should not have to
-            # worry about this hanging because the buffer is full
-            proc = subprocess.Popen(args, env=new_env, stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE, cwd=working_dir)
-            # Note that we are redirecting STDERR to STDOUT, so we can ignore
-            # the second element of the tuple returned by communicate().
-            output, serr = proc.communicate()
-            if serr:
-                if 'empty database file' not in serr:
-                    self._warning(serr)
-            if proc.returncode:
-                msg = ("clamscan could not process the file.")
-                self._warning(msg)
-                return
-            #get file in dtmp
-            regexp = re.compile(r'^clamav-[a-z0-9]{32}.tmp$')
-            for root, directories, filenames in os.walk(dtmp):
-                for filename in filenames:
-                    #not clamav tmp
-                    if not regexp.search(filename):
-                        #open and add
-                        with open(os.path.join(root,filename), 'r') as content_file_tmp:
-                            content_tmp = content_file_tmp.read()
-                            name = filename.decode('ascii', errors='ignore')
-                            stream_md5 = hashlib.md5(content_tmp).hexdigest()
-                            handle_file(name, content_tmp, obj.source,
-                                        related_id=str(obj.id),
-                                        related_type=str(obj._meta['crits_type']),
-                                        campaign=obj.campaign,
-                                        source_method=self.name,
-                                        relationship=RelationshipTypes.CONTAINED_WITHIN,
-                                        user=self.current_task.user)
-                            self._add_result('file_added', name, {'md5': stream_md5})                    
-            #remove temp dir
-            if dtmp:
+            if os.path.isdir(dtmp):
+                #create empty file for no check sig on file
+                emptyrule_path = tempfile.gettempdir() + '/emptyrule.yar'
+                if not os.path.isfile(emptyrule_path):
+                    open(emptyrule_path, 'a').close()
+                if os.path.isfile(emptyrule_path):
+                    #TODO add filename filter for avoid escape shell
+                    args = [clamscan_path, '--quiet', '--leave-temps', '--tempdir=' + dtmp, '-d', emptyrule_path, filename]
+                    #verify user can write sample
+                    acl_write = user.has_access_to(SampleACL.WRITE)
+                    if not acl_write:
+                        self._info("User does not have permission to add Sample Data to CRITs")
+                    # pdftotext does not generate a lot of output, so we should not have to
+                    # worry about this hanging because the buffer is full
+                    proc = subprocess.Popen(args, env=new_env, stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE, cwd=working_dir)
+                    # Note that we are redirecting STDERR to STDOUT, so we can ignore
+                    # the second element of the tuple returned by communicate().
+                    output, serr = proc.communicate()
+                    #print stderr without message 'empty database file'
+                    if serr:
+                        if 'empty database file' not in serr:
+                            self._warning(serr)
+                    #run command problem
+                    if proc.returncode:
+                        msg = ("clamscan could not process the file.")
+                        self._warning(msg)
+                    #run command OK
+                    else:
+                        #get file in dtmp
+                        regexp = re.compile(r'^clamav-[a-z0-9]{32}.tmp$')
+                        for root, directories, filenames in os.walk(dtmp):
+                            for filename in filenames:
+                                #not clamav tmp
+                                if not regexp.search(filename):
+                                    #open and add
+                                    with open(os.path.join(root,filename), 'r') as content_file_tmp:
+                                        content_tmp = content_file_tmp.read()
+                                        name = filename.decode('ascii', errors='ignore')
+                                        stream_md5 = hashlib.md5(content_tmp).hexdigest()
+                                        if acl_write:
+                                            handle_file(name, content_tmp, obj.source,
+                                                        related_id=str(obj.id),
+                                                        related_type=str(obj._meta['crits_type']),
+                                                        campaign=obj.campaign,
+                                                        source_method=self.name,
+                                                        relationship=RelationshipTypes.CONTAINED_WITHIN,
+                                                        user=self.current_task.user)
+                                            self._add_result('file_added', name, {'md5': stream_md5})
+                                        else:
+                                            #TODO print content for user cannot acces to write?
+                                            self._add_result('file_embedded', name, {'md5': stream_md5})                   
+                else:
+                    msg = ("ExtractEmbedded cannot create emptyrule.yar.")
+                    self._warning(msg)
+                #remove temp dir
                 shutil.rmtree(dtmp)
+            else:
+                msg = ("ExtractEmbedded cannot create temp dir.")
+                self._warning(msg)
