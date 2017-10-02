35fd9107b1e8ee2e633bb3f225acf5aa27f5eaba
lionel <lionel@cnamts.local>
2017-10-02 17:00:49 +0200

    Service extract embedded in 0.0.2, use static analysis tool

--- extract_embedded_service/__init__.py
+++ extract_embedded_service/__init__.py
@@ -1,5 +1,6 @@
 # (c) 2017, Lionel PRAT <lionel.prat9@gmail.com>
 # based on service pdf2txt of Adam Polkosnik && meta_office => Thank!
+# use tool : https://github.com/lprat/static_analysis
 # All rights reserved.
 import logging
 import hashlib
@@ -9,6 +10,7 @@ import tempfile
 import re
 from datetime import datetime
 import subprocess
+import json
 
 from django.template.loader import render_to_string
 
@@ -21,6 +23,12 @@ from crits.core.class_mapper import class_from_id
 from django.conf import settings
 from django.template.loader import render_to_string
 
+# for adding the extracted files
+from crits.screenshots.handlers import add_screenshot
+
+#get info Sample object
+from crits.samples.sample import Sample
+
 from . import forms
 
 logger = logging.getLogger(__name__)
@@ -32,7 +40,8 @@ class ExtractEmbeddedService(Service):
     """
 
     name = "ExtractEmbedded"
-    version = '0.0.1'
+    version = '0.0.2'
+    #template = "extract_embedded_service_template.html"
     supported_types = ['Sample']
     description = "Extract embedded files with clamscan."
 
@@ -47,6 +56,20 @@ class ExtractEmbeddedService(Service):
             raise ServiceConfigError("clamscan is not executable.")
         if not 'clamscan' in clamscan_path.lower():
             raise ServiceConfigError("Executable does not appear to be clamscan.")
+        analysis_path = config.get("analysis_path", "")
+        if not analysis_path:
+            raise ServiceConfigError("You must specify a valid path for analysis static tool.")
+        if not os.path.isfile(analysis_path):
+            raise ServiceConfigError("Analysis static tool path does not exist.")
+        pattern_path = config.get("pattern_path", "")
+        if not pattern_path:
+            raise ServiceConfigError("You must specify a valid path for pattern DB file.")
+        if not os.path.isfile(pattern_path):
+            raise ServiceConfigError("Pattern DB file path does not exist.")
+        yararules_path = config.get("yararules_path", "")
+        if not yararules_path:
+            raise ServiceConfigError("You must specify a valid path for yara rules.")
+        tlp_value = config.get("tlp_value", "")
 
     @staticmethod
     def get_config(existing_config):
@@ -64,7 +87,11 @@ class ExtractEmbeddedService(Service):
 
     @staticmethod
     def get_config_details(config):
-        return {'clamscan_path': config['clamscan_path']}
+        return {'clamscan_path': config['clamscan_path'],
+                'analysis_path': config['analysis_path'],
+                'pattern_path': config['pattern_path'],
+                'yararules_path': config['yararules_path'],
+                'tlp_value': config['tlp_value']}
 
     @classmethod
     def generate_config_form(self, config):
@@ -81,6 +108,31 @@ class ExtractEmbeddedService(Service):
             return False
         #work for all types
         return True
+
+    @staticmethod
+    def save_runtime_config(config):
+        if config['debug_log']:
+            del config['debug_log']
+        if config['import_file']:
+            del config['import_file']
+
+    @staticmethod
+    def bind_runtime_form(analyst, config):
+        if 'debug_log' not in config:
+            config['debug_log'] = False
+        if 'import_file' not in config:
+            config['import_file'] = False
+        form = forms.ExtractEmbeddedRunForm(data=config)
+        return form
+
+    @classmethod
+    def generate_runtime_form(self, analyst, config, crits_type, identifier):
+        html = render_to_string("services_run_form.html",
+                                {'name': self.name,
+                                 'form': forms.ExtractEmbeddedRunForm(),
+                                 'crits_type': crits_type,
+                                 'identifier': identifier})
+        return html
         
     def run(self, obj, config):
         obj.filedata.seek(0)
@@ -90,72 +142,150 @@ class ExtractEmbeddedService(Service):
         self.config = config
         self.obj = obj
         self._debug("ExtractEmbedded started")
-        clamscan_path = self.config.get("clamscan_path", "/usr/bin/clamscan")
+        tlp_value = self.config.get("tlp_value", "tlp_value")
+        clamscan_path = self.config.get("clamscan_path", os.path.dirname(os.path.realpath(__file__))+'/static_analysis/clamav-devel/clamscan/clamscan')
+        analysis_path = self.config.get("analysis_path", os.path.dirname(os.path.realpath(__file__))+'/static_analysis/analysis.py')
+        yararules_path = self.config.get("yararules_path", os.path.dirname(os.path.realpath(__file__))+'/static_analysis/yara_rules/')
+        pattern_path = self.config.get("pattern_path", os.path.dirname(os.path.realpath(__file__))+'/static_analysis/pattern.db')
         #write out the sample stored in the db to a tmp file
         with self._write_to_file() as tmp_file:
             (working_dir, filename) = os.path.split(tmp_file)
             new_env = dict(os.environ)  # Copy current environment
             args = []
             obj.filedata.seek(0)
-            #make temp dir for extract embedded file
-            dtmp = tempfile.mkdtemp()
-            if os.path.isdir(dtmp):
-                #create empty file for no check sig on file
-                emptyrule_path = tempfile.gettempdir() + '/emptyrule.yar'
-                if not os.path.isfile(emptyrule_path):
-                    open(emptyrule_path, 'a').close()
-                if os.path.isfile(emptyrule_path):
-                    #TODO add filename filter for avoid escape shell
-                    args = [clamscan_path, '--quiet', '--leave-temps', '--tempdir=' + dtmp, '-d', emptyrule_path, filename]
-                    #verify user can write sample
-                    acl_write = user.has_access_to(SampleACL.WRITE)
-                    if not acl_write:
-                        self._info("User does not have permission to add Sample Data to CRITs")
-                    # pdftotext does not generate a lot of output, so we should not have to
-                    # worry about this hanging because the buffer is full
-                    proc = subprocess.Popen(args, env=new_env, stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE, cwd=working_dir)
-                    # Note that we are redirecting STDERR to STDOUT, so we can ignore
-                    # the second element of the tuple returned by communicate().
-                    output, serr = proc.communicate()
-                    #print stderr without message 'empty database file'
-                    if serr:
-                        if 'empty database file' not in serr:
-                            self._warning(serr)
-                    #run command problem
-                    if proc.returncode:
-                        msg = ("clamscan could not process the file.")
-                        self._warning(msg)
-                    #run command OK
-                    else:
-                        #get file in dtmp
-                        regexp = re.compile(r'^clamav-[a-z0-9]{32}.tmp$')
-                        for root, directories, filenames in os.walk(dtmp):
-                            for filename in filenames:
-                                #not clamav tmp
-                                if not regexp.search(filename):
-                                    #open and add
-                                    with open(os.path.join(root,filename), 'r') as content_file_tmp:
-                                        content_tmp = content_file_tmp.read()
-                                        name = filename.decode('ascii', errors='ignore')
-                                        stream_md5 = hashlib.md5(content_tmp).hexdigest()
-                                        if acl_write:
-                                            handle_file(name, content_tmp, obj.source,
-                                                        related_id=str(obj.id),
-                                                        related_type=str(obj._meta['crits_type']),
-                                                        campaign=obj.campaign,
-                                                        source_method=self.name,
-                                                        relationship=RelationshipTypes.CONTAINED_WITHIN,
-                                                        user=self.current_task.user)
-                                            self._add_result('file_added', name, {'md5': stream_md5})
-                                        else:
-                                            #TODO print content for user cannot acces to write?
-                                            self._add_result('file_embedded', name, {'md5': stream_md5})                   
-                else:
-                    msg = ("ExtractEmbedded cannot create emptyrule.yar.")
-                    self._warning(msg)
-                #remove temp dir
-                shutil.rmtree(dtmp)
+            #make temp file for get json result and graph
+            dirtmp = tempfile._get_default_tempdir()
+            file_png=dirtmp+'/'+next(tempfile._get_candidate_names())+'.png'
+            file_json=dirtmp+'/'+next(tempfile._get_candidate_names())+'.json'
+            if os.path.isfile(file_png) or os.path.isfile(file_json):
+                self._warning('Error: File temp exist.')
             else:
-                msg = ("ExtractEmbedded cannot create temp dir.")
-                self._warning(msg)
+                #TODO add choice: verbose mode and extracted file emmbed
+                args = ['python', analysis_path, '-c', clamscan_path, '-g', '-y', yararules_path, '-p', pattern_path, '-f', tmp_file, '-s', file_png, '-j', file_json]
+                if config['debug_log']:
+                    args = ['python', analysis_path, '-c', clamscan_path, '-g', '-y', yararules_path, '-p', pattern_path, '-f', tmp_file, '-s', file_png, '-j', file_json, '-v']
+                #verify user can write sample
+                acl_write = user.has_access_to(SampleACL.WRITE)
+                if not acl_write:
+                    self._info("User does not have permission to add Sample Data to CRITs")
+                # pdftotext does not generate a lot of output, so we should not have to
+                # worry about this hanging because the buffer is full
+                proc = subprocess.Popen(args, env=new_env, stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE, cwd=working_dir)
+                # Note that we are redirecting STDERR to STDOUT, so we can ignore
+                # the second element of the tuple returned by communicate().
+                output, serr = proc.communicate()
+                #print stderr without message 'empty database file'
+                if serr:
+                    self._warning(serr)
+                #run command problem
+                if proc.returncode:
+                    msg = ("analysis could not process the file.")
+                    self._warning(msg)
+                    if os.path.isfile(file_json):
+                        os.remove(file_json)
+                    if os.path.isfile(file_png):
+                        os.remove(file_png)         
+                #run command OK
+                else:
+                    #add json information and png file
+                    result_extract = None
+                    if os.path.isfile(file_json):
+                        with open(file_json) as data_file:
+                            try:
+                                result_extract = json.load(data_file)
+                            except Exception as e:
+                                self._warning("Error to parse json result: "+str(e))
+                        os.remove(file_json)
+                    if os.path.isfile(file_png):
+                        #add screeshot
+                        fileh = open(file_png, "rb")
+                        fileh.seek(0)
+                        res = add_screenshot(description='Render of analysis embedded files',
+                                                     tags=None,
+                                                     method=self.name,
+                                                     source=obj.source,
+                                                     reference=None,
+                                                     analyst=self.current_task.user.username,
+                                                     screenshot=fileh,
+                                                     screenshot_ids=None,
+                                                     oid=obj.id,
+                                                     tlp=tlp_value,
+                                                     otype="Sample")
+                        if res.get('message') and res.get('success') == True:
+                            self._warning("res-message: %s id:%s" % (res.get('message'), res.get('id') ) )
+                            self._add_result('Graph analysis', res.get('id'), {'Message': res.get('message')})
+                        self._info("id:%s, file: %s" % (res.get('id'), file_png))
+                        fileh.close()
+                        os.remove(file_png)
+                    if type(result_extract) is dict:
+                        parse_result(self, result_extract, acl_write, None)
+                        if result_extract['TempDirExtract'] and dirtmp in str(result_extract['TempDirExtract']) and os.path.isdir(str(result_extract['TempDirExtract'])):
+                            #remove dir tmp
+                            shutil.rmtree(result_extract['TempDirExtract'])
+   
+def parse_result(self, result_extract, acl_write, md5_parent):
+    stream_md5 = None
+    if type(result_extract) is dict:
+        #make reccursion extract each file embbed
+        if 'FileMD5' in result_extract and result_extract['FileMD5']:
+            tmp_dict={}
+            #extract info
+            no_info=['ExtractInfo','ContainedObjects', 'Yara', 'PathFile', 'FileMD5', 'RootFileType', 'TempDirExtract']
+            for key, value in result_extract.iteritems():
+                if not key in no_info:
+                    self._add_result('File: '+result_extract['FileMD5'] + ' - Info', key, {'value': str(value)})
+            #extract yara match
+            if result_extract['Yara']:
+                for item_v in result_extract['Yara']:
+                    for key, value in item_v.iteritems():
+                        self._add_result('File: '+result_extract['FileMD5'] + ' - Signatures yara matched', key, value)
+            #extract IOC
+            if result_extract['ExtractInfo']:
+                for item_v in result_extract['ExtractInfo']:
+                    for key, value in item_v.iteritems():
+                        self._add_result('File: '+result_extract['FileMD5'] + ' - Extract potential IOC', key, {'value': str(value)})
+            #add_sample
+            if 'PathFile' in result_extract and type(result_extract['PathFile']) is list and len(result_extract['PathFile']) > 0:
+                if os.path.isfile(str(result_extract['PathFile'][0])):
+                    with open(str(result_extract['PathFile'][0]), 'r') as content_file_tmp:                        
+                        content_tmp = content_file_tmp.read()
+                        stream_md5 = hashlib.md5(content_tmp).hexdigest()
+                        name = str(stream_md5).decode('ascii', errors='ignore')
+                        id_ = Sample.objects(md5=stream_md5).only('id').first()
+                        if id_:
+                            self._add_result('Samples added', name, {'samples': stream_md5, 'exists': 'Yes id:'+str(id_.id)})
+                            #make relationship
+                            id_.add_relationship(rel_item=self.obj,
+                                     rel_type=RelationshipTypes.CONTAINED_WITHIN,
+                                     rel_date=datetime.now(),
+                                     analyst=self.current_task.user.username)
+                        else:
+                            if acl_write and self.config['import_file']:
+                                obj_parent = None
+                                if md5_parent:
+                                    obj_parent = Sample.objects(md5=md5_parent).only('id').first()
+                                if not obj_parent:
+                                    sample = handle_file(name, content_tmp, self.obj.source,
+                                            related_id=str(self.obj.id),
+                                            related_type=str(self.obj._meta['crits_type']),
+                                            campaign=self.obj.campaign,
+                                            source_method=self.name,
+                                            relationship=RelationshipTypes.CONTAINED_WITHIN,
+                                            user=self.current_task.user)
+                                else:
+                                    sample = handle_file(name, content_tmp, obj_parent.source,
+                                            related_id=str(obj_parent.id),
+                                            related_type=str(obj_parent._meta['crits_type']),
+                                            campaign=obj_parent.campaign,
+                                            source_method=self.name,
+                                            relationship=RelationshipTypes.CONTAINED_WITHIN,
+                                            user=self.current_task.user)
+                                self._add_result('Samples added', name, {'samples': stream_md5, 'exists': 'No'})
+
+            #contains file
+            if 'ContainedObjects' in result_extract and type(result_extract['ContainedObjects']) is list and result_extract['ContainedObjects']:
+                for item_v in result_extract['ContainedObjects']:
+                    if item_v['FileMD5'] and item_v['FileType'] and item_v['FileSize']:
+                        self._add_result('File: '+result_extract['FileMD5'] + ' - Contains md5 files', item_v['FileMD5'], {'type': str(item_v['FileType']), 'size': str(item_v['FileSize'])})
+                    parse_result(self, item_v, acl_write, stream_md5)
--- extract_embedded_service/forms.py
+++ extract_embedded_service/forms.py
@@ -1,3 +1,4 @@
+import os
 from django import forms
 
 class ExtractEmbeddedConfigForm(forms.Form):
@@ -5,10 +6,45 @@ class ExtractEmbeddedConfigForm(forms.Form):
     required_css_class = 'required'
     clamscan_path = forms.CharField(required=True,
                                label="clamscan binary",
-                               initial='/usr/bin/clamscan',
+                               initial=os.path.dirname(os.path.realpath(__file__))+'/static_analysis/clamav-devel/clamscan/clamscan',
                                widget=forms.TextInput(),
                                help_text="Full path to clamscan binary.")
-
+    analysis_path = forms.CharField(required=True,
+                               label="Analysis static tool",
+                               initial=os.path.dirname(os.path.realpath(__file__))+'/static_analysis/analysis.py',
+                               widget=forms.TextInput(),
+                               help_text="Full path to analysis static tool. (https://github.com/lprat/static_analysis)")
+    yararules_path = forms.CharField(required=True,
+                               label="Yara rules",
+                               initial=os.path.dirname(os.path.realpath(__file__))+'/static_analysis/yara_rules/',
+                               widget=forms.TextInput(),
+                               help_text="Yara rules path for analysis static tool.")
+    pattern_path = forms.CharField(required=True,
+                               label="Pattern DB",
+                               initial=os.path.dirname(os.path.realpath(__file__))+'/static_analysis/pattern.db',
+                               widget=forms.TextInput(),
+                               help_text="Pattern DB path for extract data")
+    tlp_value = forms.CharField(required=True,
+                               label="Tlp value",
+                               initial='red',
+                               widget=forms.TextInput(),
+                               help_text="Indicate TLP value.")                            
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', ':')
         super(ExtractEmbeddedConfigForm, self).__init__(*args, **kwargs)
+
+class ExtractEmbeddedRunForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    debug_log = forms.BooleanField(required=False,
+                                    initial=False,
+                                    label="Debug",
+                                    help_text="Insert debug info in log.")
+    import_file = forms.BooleanField(required=False,
+                                    initial=True,
+                                    label="Import",
+                                    help_text="Import extracted file in CRITS as sample.")
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', ':')
+        super(ExtractEmbeddedRunForm, self).__init__(*args, **kwargs)
